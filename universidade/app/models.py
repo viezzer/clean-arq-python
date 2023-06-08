9c41dd1d9b73bd5e36a3baef48443df8dc71a571
eduviezzer <33733608+viezzer@users.noreply.github.com>
2023-06-08 16:00:14 -0300

    Criado resto das classes

--- universidade/app/models.py
+++ universidade/app/models.py
@@ -2,10 +2,10 @@ from django.db import models
 import uuid
 
 # Create your models here.
-class Estudante(models.Model):
+
+class Disciplina(models.Model):
+    codigo = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     nome = models.CharField(max_length=30)
-    sobrenome = models.CharField(max_lenght=30)
-    email = models.EmailField()
 
     def __str__(self):
         return self.nome
@@ -13,3 +13,23 @@ class Estudante(models.Model):
 class Curso(models.Model):
     codigo = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     nome = models.CharField(max_length=30)
+    disciplinas = models.ManyToManyField(Disciplina)
+
+    def __str__(self):
+        return self.nome
+
+
+class Estudante(models.Model):
+    nome = models.CharField(max_length=30)
+    sobrenome = models.CharField(max_length=30)
+    email = models.EmailField()
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+    
+class Matricula(models.Model):
+    estudante = models.ForeignKey(Estudante, on_delete=models.CASCADE)
+    disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
+    data = models.DateField()
+    
