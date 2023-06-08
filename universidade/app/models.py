b96ab7be00e8343e0897f87ec2f4da3cfabd025e
eduviezzer <33733608+viezzer@users.noreply.github.com>
2023-06-08 15:31:24 -0300

    Criado modelos de estudante e curso

--- universidade/app/models.py
+++ universidade/app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Estudante(models.Model):
+    nome = models.CharField(max_length=30)
+    sobrenome = models.CharField(max_lenght=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nome
+    
+class Curso(models.Model):
+    codigo = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nome = models.CharField(max_length=30)
